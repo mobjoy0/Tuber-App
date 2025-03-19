1ee8b409979b05a3a4876f1eede93bfd25fea4ae
mobjoy0 <wessimbenna@hotmail.com>
2025-03-19 17:36:54 +0100

    initial commit

---
+++ Tuber_backend/src/main/java/com/project/Tuber_backend/TuberBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.Tuber_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuberBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuberBackendApplication.class, args);
+	}
+
+}
---
+++ Tuber_backend/src/test/java/com/project/Tuber_backend/TuberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Tuber_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuberBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
